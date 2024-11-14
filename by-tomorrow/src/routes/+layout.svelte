93a239e6bffeb69e1875cade49a562babb113ffe
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-11-14 12:53:35 -0500

    add about page and nav to layout header>

--- by-tomorrow/src/routes/+layout.svelte
+++ by-tomorrow/src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import '../app.css'
+	let { children } = $props()
 </script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
 
 {@render children()}
