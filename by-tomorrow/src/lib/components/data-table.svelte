f1577c727e9e7b9f038c6cd262ffefea2bf94971
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-11-27 17:12:03 -0500

    make a data table comp, add a header, add to library page

---
+++ by-tomorrow/src/lib/components/data-table.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table/index'
+	const data_headers = ['Name', 'Age', 'City']
+	const data = [
+		{ name: 'Alice', age: 25, city: 'New York' },
+		{ name: 'Bob', age: 30, city: 'Los Angeles' },
+		{ name: 'Charlie', age: 35, city: 'Chicago' },
+		{ name: 'David', age: 40, city: 'Houston' },
+		{ name: 'Eve', age: 45, city: 'Phoenix' },
+	]
+</script>
+
+<Table.Root>
+	<Table.Header>
+		<Table.Row>
+			{#each data_headers as header}
+				<Table.Head class="text-lg tracking-tight  text-balance"
+					>{header}</Table.Head
+				>
+			{/each}
+		</Table.Row>
+	</Table.Header>
+</Table.Root>
--- by-tomorrow/src/routes/library/+page.svelte
+++ by-tomorrow/src/routes/library/+page.svelte
@@ -1,1 +1,17 @@
-<h2>Library Mode Coming Soon</h2>
+<script lang="ts">
+	import DataTable from '$lib/components/data-table.svelte'
+	import Label from '$lib/components/ui/label/label.svelte'
+</script>
+
+<div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+	<div class="flex items-center justify-between space-y-4">
+		<div>
+			<!-- leading-tight lg:leading-[1.1] -->
+			<h2 class="text-2xl font-bold tracking-tight text-balance">
+				Source Library
+			</h2>
+
+			<DataTable />
+		</div>
+	</div>
+</div>
