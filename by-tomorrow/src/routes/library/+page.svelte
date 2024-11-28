f7296a1e12180113d494c45eae52b5510b36419e
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-11-28 00:21:09 -0500

    update library page formatting and add descriptive text

--- by-tomorrow/src/routes/library/+page.svelte
+++ by-tomorrow/src/routes/library/+page.svelte
@@ -3,12 +3,17 @@
 	import Label from '$lib/components/ui/label/label.svelte'
 </script>
 
-<div>
-	<div>
-		<h2 class="text-2xl font-bold tracking-tight text-balance">
-			Source Library
-		</h2>
-
+<div class="hidden h-full flex-1 flex-col px-4 md:flex">
+	<div class="flex flex-col items-center justify-between space-y-2 gap-4">
+		<div>
+			<h2 class="text-2xl font-bold tracking-tight text-balance">
+				Source Library
+			</h2>
+			<p class="text-muted-foreground">
+				Upload and explore source documents (eg. pdfs, blogposts). Then select
+				some to annotate in the reader.
+			</p>
+		</div>
 		<DataTable />
 	</div>
 </div>
