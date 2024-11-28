f3c69bb5b9676028a5691ca0b94e8c79beb13f4d
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-11-28 01:48:58 -0500

    update the dialog trigger to look like a button

--- by-tomorrow/src/lib/components/file-load-dialog.svelte
+++ by-tomorrow/src/lib/components/file-load-dialog.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import * as Dialog from '$lib/components/ui/dialog/index.js'
+	import { Button, buttonVariants } from '$lib/components/ui/button/index'
+	import FileUp from 'lucide-svelte/icons/file-up'
 </script>
 
 <Dialog.Root>
-	<Dialog.Trigger>Upload File</Dialog.Trigger>
+	<Dialog.Trigger class={buttonVariants({ variant: 'default' })}>
+		<FileUp /> Add File
+	</Dialog.Trigger>
 	<Dialog.Content>
 		<Dialog.Header>
 			<Dialog.Title>Upload File</Dialog.Title>
