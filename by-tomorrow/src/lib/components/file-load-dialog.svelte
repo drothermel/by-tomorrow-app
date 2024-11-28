c26ba7e74f805f95ecddf3e264a80a1fd9c7fe84
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-11-28 02:31:18 -0500

    add arxiv dummy card, tabs to upload dialog

--- by-tomorrow/src/lib/components/file-load-dialog.svelte
+++ by-tomorrow/src/lib/components/file-load-dialog.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import LocalFileCard from '$lib/components/load/local-file-card.svelte'
-	import * as Dialog from '$lib/components/ui/dialog/index.js'
+	import ArixvCard from '$lib/components/load/arxiv-card.svelte'
+	import * as Dialog from '$lib/components/ui/dialog/index'
+	import * as Tabs from '$lib/components/ui/tabs/index'
 	import { Button, buttonVariants } from '$lib/components/ui/button/index'
 	import FileUp from 'lucide-svelte/icons/file-up'
+	import ArxivCard from '$lib/components/load/arxiv-card.svelte'
 
 	function handleUpload() {
 		console.log('Upload file!')
@@ -14,9 +17,17 @@
 		<FileUp /> Add File
 	</Dialog.Trigger>
 	<Dialog.Content>
-		<Dialog.Header>
-			<Dialog.Title>Upload File</Dialog.Title>
-			<LocalFileCard {handleUpload} />
-		</Dialog.Header>
+		<div class="justify-center flex">
+			<Tabs.Root value="local" class="w-[400px]">
+				<Tabs.List class="grid w-full grid-cols-2">
+					<Tabs.Trigger value="local">Local File</Tabs.Trigger>
+					<Tabs.Trigger value="arxiv">Arxiv URL</Tabs.Trigger>
+				</Tabs.List>
+				<Tabs.Content value="local">
+					<LocalFileCard {handleUpload} />
+				</Tabs.Content>
+				<Tabs.Content value="arxiv"><ArxivCard /></Tabs.Content>
+			</Tabs.Root>
+		</div>
 	</Dialog.Content>
 </Dialog.Root>
