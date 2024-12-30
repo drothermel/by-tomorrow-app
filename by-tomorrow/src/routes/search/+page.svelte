2a113d1f94efbbb192da382dbee09737e63b64a6
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-12-30 16:05:06 -0500

    add the JSON viewer page from frontend_practice and updated formatting slightly

--- by-tomorrow/src/routes/search/+page.svelte
+++ by-tomorrow/src/routes/search/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+    import ArxivHandler from "$lib/components/arxiv.svelte";
+    import { JsonView } from "@zerodevx/svelte-json-view";
 
+    import { Button } from "$lib/components/ui/button/index";
+    import { Input } from "$lib/components/ui/input/index";
+    import { Label } from "$lib/components/ui/label/index.js";
+
+    let handler = $state(new ArxivHandler());
 </script>
 
 <div class="flex h-full flex-1 flex-col items-center px-4">
@@ -16,5 +23,42 @@
 				</p>
 			</div>
 		</div>
+        <div class="flex flex-col gap-4">
+            <div class="flex flex-row gap-4">
+              <Label for="maxRes">Max Results:</Label>
+              <Input
+                id="maxRes"
+                type="number"
+                bind:value={handler.currQuery.maxResults}
+              />
+            </div>
+            <Label for="query_author">Query Arxiv for Author Papers:</Label>
+            <Input
+              id="query_author"
+              type="text"
+              bind:value={handler.currQuery.author}
+              placeholder="Author Name"
+            />
+            <Button onclick={() => handler.queryArxiv()}>
+              Query Arxiv API
+            </Button>
+        
+            <Label for="results">Retrieved Arxiv Results:</Label>
+            <div id="results">
+              {#if handler.resultFeed.length > 0}
+                <div class="flex flex-col gap-4 w-full prose-sm p-4 bg-slate-50">
+                  {#each handler.resultFeed as entry}
+                    <div class="flex flex-col p-4 gap-2 bg-slate-100">
+                      <JsonView json={entry} />
+                    </div>
+                  {/each}
+                </div>
+              {:else}
+                <div class="p-4 prose-sm bg-slate-50">
+                  <p>No Results to Show</p>
+                </div>
+              {/if}
+            </div>
+          </div>
 	</div>
 </div>
