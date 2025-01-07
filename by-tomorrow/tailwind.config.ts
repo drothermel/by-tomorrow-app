77b46061fe0a11e364e1241a081905e97d2c12b9
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-01-07 13:29:31 -0500

    overwrite the typography width settings

--- by-tomorrow/tailwind.config.ts
+++ by-tomorrow/tailwind.config.ts
@@ -89,6 +89,13 @@ const config: Config = {
         		"accordion-up": "accordion-up 0.2s ease-out",
        			"caret-blink": "caret-blink 1.25s ease-out infinite",
       		},
+			typography: {
+				DEFAULT: {
+					css: {
+						maxWidth: "none",
+					}
+				}
+			}
 		},
 	},
 	plugins: [tailwindcssAnimate, typography],
