0b937ac4a6df9139c7ad7aeef3078a443f8fbec9
Danielle Rothermel <33494540+drothermel@users.noreply.github.com>
2025-05-24 18:16:31 -0400

    Remove unused components

--- by-tomorrow/src/lib/components/nav-user.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import * as Avatar from '$lib/components/ui/avatar/index'
-	import * as Sidebar from '$lib/components/ui/sidebar/index'
-
-	let { user }: { user: { name: string; avatar: string } } = $props()
-</script>
