4b96ea0dbec6235c4469ac25fe814041bb1f2c0c
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-09-20 22:21:42 -0400

    add serverside protection for protected pages and make an admin page

--- src/routes/(protected)/+layout.server.ts
+++ src/routes/(protected)/+layout.server.ts
@@ -1,8 +1,15 @@
-import { buildClerkProps } from 'svelte-clerk/server'
+import { redirect } from '@sveltejs/kit'
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = ({ locals }) => {
-    return {
-        ...buildClerkProps(locals.auth())
+export const load: LayoutServerLoad = async ({ locals, parent }) => {
+    await parent() // ensures proper load order
+
+    const { userId } = locals.auth()
+    if (!userId) {
+        throw redirect(307, '/sign-in')
     }
+
+    return {
+        userId
+    };
 }
